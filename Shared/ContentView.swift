a591e6562158f8cc19f57fa01fb6caeda66a22b3
DumitruBogdan <90133050+DumitruBogdan@users.noreply.github.com>
2022-04-12 12:54:38 +0300

    Implement converter app

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    var units = ["Temperature", "Length", "Time", "Volume"]
+    var temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
+    var lengthUnits = ["meters", "kilometers", "feet", "yards", "miles"]
+    var timeUnits = ["seconds", "minutes", "hours", "days"]
+    var volumeUnits = ["milliliters", "liters", "cups", "pints", "gallons"]
+    @State var unitSelected = "Length"
+    @State var inputUnit = "Meters"
+    @State var outputUnit = "Kilometers"
+    @State var inputValue = 0.0
+    @State var outputValue = 2.0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section{
+                    Picker("Select the desired unit", selection: $unitSelected){
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                HStack{
+                    TextField("The value is: ", value:$inputValue, format: .number)
+                        .multilineTextAlignment(.center)
+                        .keyboardType(.decimalPad)
+                    TextField("The output is: ", value:$outputValue, format: .number)
+                        .multilineTextAlignment(.center)
+                        .disabled(true)
+                }
+                
+                Section{
+                    Picker("From", selection:$inputUnit){
+                        switch unitSelected {
+                        case "Temperature":
+                            ForEach(temperatureUnits, id: \.self){
+                                Text($0)
+                            }
+                        case "Time":
+                            ForEach(timeUnits, id: \.self){
+                                Text($0)
+                            }
+                        case "Volume":
+                            ForEach(volumeUnits, id: \.self){
+                                Text($0)
+                            }
+                        default:
+                            ForEach( lengthUnits, id: \.self){
+                                Text($0)
+                            }
+                        }
+                    }
+                }
+                Section{
+                    Picker("To", selection:$outputUnit){
+                        switch unitSelected {
+                        case "Temperature":
+                            ForEach(temperatureUnits, id: \.self){
+                                Text($0)
+                            }
+                        case "Time":
+                            ForEach(timeUnits, id: \.self){
+                                Text($0)
+                            }
+                        case "Volume":
+                            ForEach(volumeUnits, id: \.self){
+                                Text($0)
+                            }
+                        default:
+                            ForEach( lengthUnits, id: \.self){
+                                Text($0)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Converter")
+        }
+        .onChange(of: self.inputValue) { newValue in
+            outputValue = lengthConverter(inputType: inputUnit, inputValue: newValue, outputType: outputUnit)
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Shared/LengthConvertor.swift
@@ -0,0 +1,37 @@
+//
+//  LengthConvertor.swift
+//  ConverterAPP
+//
+//  Created by Bogdan on 12.04.2022.
+//
+
+import Foundation
+
+func lengthConverter(inputType: String, inputValue: Double, outputType: String) -> Double {
+    var aux: Double
+    switch inputType{
+    case "kilometers":
+        aux = inputValue * 1000
+    case "feet":
+        aux = inputValue / 3.281
+    case "yards":
+        aux = inputValue / 1.094
+    case "miles":
+        aux = inputValue * 1609
+    default:
+        aux = inputValue
+    }
+    switch outputType{
+    case "kilometers":
+        aux = inputValue / 1000
+    case "feet":
+        aux = inputValue * 3.281
+    case "yards":
+        aux = inputValue * 1.094
+    case "miles":
+        aux = inputValue / 1609
+    default:
+        aux = inputValue
+    }
+    return aux
+}
