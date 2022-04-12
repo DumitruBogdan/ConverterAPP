70d36fc81a6a61d6c3126bd2196cb794976113ec
DumitruBogdan <90133050+DumitruBogdan@users.noreply.github.com>
2022-04-12 09:58:44 +0300

    Initial Commit

---
+++ Shared/ConverterAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterAPPApp.swift
+//  Shared
+//
+//  Created by Bogdan on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConverterAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
